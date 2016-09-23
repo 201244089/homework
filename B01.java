e18f51e457f1bc2b89e37b248868ff69728a8a13
LBH <dlqudgml0503@naver.com>
2016-09-23 12:27:49 +0900

    gitignore 파일 추가

---
+++ B01.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class B01{
+	int age;
+	int birth_year;
+	void input(){
+		Scanner s = new scanner(System.in);
+		System.out.println("Input your birth year....");
+		birth_year
+	
+	}
+
+
+	public static void main(String[] s){
+		new B01().print();
+	}
+
+
+}
+
